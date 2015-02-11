f5d8fdc4ee10e36bee6681264b3394d754891245
Brian Stocking <brianm.stocking@gmail.com>
2015-02-10 19:21:59 -0700

    finished

--- service.js
+++ service.js
@@ -1,12 +1,18 @@
 var app = angular.module('userProfiles');
 
-app.service('mainService', function($http, $q) {
-
-
-  this.getUsers = function() {
-    return $http({
-        method: 'GET',
-        url: 'http://reqr.es/api/users?page=1'
+app.service('mainService', function ($http, $q) {
+  var deferred = $q.defer();
+  this.getUsers = function () {
+     $http({
+      method: 'GET',
+      url: 'http://reqr.es/api/users?page=1'
+    }).then(function (response) {
+      response = response.data.data
+      deferred.resolve(response)
+           for(var i = 0; i < response.length; i++) {
+        response[i].first_name = 'Ralf'
+      }
     })
+    return deferred.promise;
   }
 });
